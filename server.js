6be50676a0997049a7c696a6a960228eb4bb502b
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-03 15:42:28 +0100

    Work with static endpoints

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var app = express();
 
+
+app.get('/', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony głównej');
+    res.send('Hello GET!');
+});
+
+app.post('/', function (req, res) {
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('Hello POST!');
+});
+
+app.delete('/del_user', function (req, res) {
+    console.log('Otrzymałem żądanie DELETE do strony /del_user');
+    res.send('Hello DELETE!');
+});
+
+app.get('/list_user', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników!');
+});
+
+app.get('/ab*cd', function(req, res) {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
+});
+
 var server = app.listen(3000, function() {
     console.log('Przyładowa app nasłuchuje na localhost:3000')
 })
-
-app.get('/', function (req, res) {
-    res.send('Hello world');
-})
-
