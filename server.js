ff9969361a777327111e9878768405e58499441e
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-04 10:49:32 +0100

    Work with 404 error

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/:id', function (req, res) {
-    res.send('Identyfikator ' + req.params.id);
+app.get('/', function (req, res) {
+    res.send('Hello world!');
 });
 
 app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
