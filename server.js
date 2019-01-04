451d1a864b5f813d9c611f5c7c168a2677236c0a
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-04 10:46:20 +0100

    Work with dynamic endpoints

--- server.js
+++ server.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var app = express();
 
-
-app.get('/', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony głównej');
-    res.send('Hello GET!');
+app.get('/:id', function (req, res) {
+    res.send('Identyfikator ' + req.params.id);
 });
 
-app.post('/', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Hello POST!');
-});
-
-app.delete('/del_user', function (req, res) {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    res.send('Hello DELETE!');
-});
-
-app.get('/list_user', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.get('/ab*cd', function(req, res) {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-});
-
-var server = app.listen(3000, function() {
-    console.log('Przyładowa app nasłuchuje na localhost:3000')
-})
+app.listen(3000);
