a756ed940534f6de09fd2e1a5746cb11e501e262
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-06 13:07:00 +0100

    Exercise with pug templates and its options

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views','./views');
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store');
-    next();
+
+app.get('/', function(req, res){
+    res.render('main');
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+app.get('/main', function(req, res){
+    res.render('main');
 });
 
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
+app.get('/log-in', function(req, res){
+    res.render('log-in');
 });
 
-app.get('/first-template', function(req, res){
-    res.render('first-template');
-});
-
-app.get('/dynamic-view', function(req, res){
-    res.render('dynamic', {
-        name: "Moja dynamiczna strona",
-        url: "http://www.google.com"
-    });
+app.get('/logged', function(req, res){
+    res.render('logged');
 });
 
 app.listen(3000);
 app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+    res.status(404).render('error')
 });
